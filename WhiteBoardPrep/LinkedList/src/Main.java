fd08f90629c0320f90d84a1e625e60192859dbde
Anu Tomy <anuatomy@gmail.com>
2018-01-22 22:17:45 -0500

    Examples of LinkedList

--- WhiteBoardPrep/LinkedList/src/Main.java
+++ WhiteBoardPrep/LinkedList/src/Main.java
@@ -17,8 +17,8 @@ public class Main {
         linkedList.add("Egg");
         linkedList.add("Bread");
 
-        /*Display Linked List Content*/
-        System.out.println("Linked List Content: " + linkedList);
+        //Display Linked List Content
+        System.out.println("Linked List elements: " + linkedList);
 
         //Add First and Last Element
         linkedList.addFirst("First Item");
@@ -60,9 +60,17 @@ public class Main {
 
         //make a copy of the list
         Object cpy = linkedList.clone();
-        System.out.println("Copy of Linkedlist is : " + cpy);
-
-
-
+        System.out.println("Copy of LinkedList is : " + cpy);
     }
 }
+/* Linked List elements: [Milk, Cheese, Egg, Bread]
+LinkedList Content after addition: [First Item, Milk, Cheese, Egg, Bread, Last Item]
+First element: First Item
+First element after update by set method: Changed first element
+LinkedList after deletion of first and last element: [Milk, Cheese, Egg, Bread]
+New List: [Milk, Newly added item, Cheese, Egg, Bread]
+After adding Array elements: [Milk, Newly added item, Cheese, Egg, Bread, Chips, Chicken, Carrot]
+Final Contents in the list: [Milk, Newly added item, Cheese, Bread, Chips, Chicken, Carrot]
+Number of elements in this list: 7
+Copy of LinkedList is : [Milk, Newly added item, Cheese, Bread, Chips, Chicken, Carrot]
+*/
